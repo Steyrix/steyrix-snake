a09250cf081e2a955b6d6c62cbc0d4c1c2c2a2e5
Georgy Pevnev <steyrixio@gmail.com>
2019-03-09 01:33:22 +0300

    GameScene implementation, draw snake and food

--- src/game/scenes/GameScene.java
+++ src/game/scenes/GameScene.java
@@ -1,6 +1,8 @@
 package game.scenes;
 
 import game.Drawable;
+import game.snakecore.Snake;
+import game.snakecore.SnakeFood;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,69 +12,132 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class GameScene implements Scene {
 
-    private static int PANEL_WIDTH = 500;
-    private static int PANEL_HEIGHT = 500;
 
-    private static final int squareSize = 50;
-    private final ArrayList<Drawable> objects;
+    private static final int PANEL_WIDTH = 500;
+    private static final int PANEL_HEIGHT = 500;
 
-    public GameScene(){
-        objects = new ArrayList<>();
+    private final ArrayList<Drawable> drawables;
 
+    private final Snake snake;
+    private SnakeFood foodItem;
+    private String pointStr;
+    private Snake.Direction dir;
+    private int previousKeyCode;
+    private int points;
+    private final int squareSize = 50;
+
+    public GameScene() {
+        drawables = new ArrayList<>();
+        snake = new Snake(3, new Point(250, 250), Snake.Direction.UP);
+        drawables.add(snake);
         init();
     }
 
     @Override
     public void init() {
 
+        points = 0;
+        pointStr = "(ESC to pause) Points: ";
+        generateFood();
+    }
+
+    private void renderDrawables(Graphics g) {
+        for (Drawable dObj : drawables) {
+            dObj.draw((Graphics2D) g);
+        }
     }
 
     @Override
     public void drawScene(Graphics g) {
-
+        renderDrawables(g);
+        drawPlayField(g);
     }
 
     @Override
     public void actionPerformed() {
+        snake.moveSnake();
 
+        if (snake.intersects(foodItem)) {
+            snake.addBodyPart();
+            points++;
+            generateFood();
+        }
+
+        if (snake.intersects(snake)) {
+            snake.setDeathClr();
+            snake.stop();
+        }
+
+        if (snake.getPosition().x >= PANEL_WIDTH
+                || snake.getPosition().x <= 0 - snake.getWidth()
+                || snake.getPosition().y >= PANEL_HEIGHT
+                || snake.getPosition().y <= 0 - snake.getHeight()) {
+            snake.setDeathClr();
+            snake.stop();
+        }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            pointStr = "(Paused. ENTER to resume / ESC to exit) Points: ";
+            snake.stop();
+            dir = snake.getCurrDir();
+        } else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            if (previousKeyCode == KeyEvent.VK_ESCAPE) {
+                pointStr = "(ESC to pause) Points: ";
+                snake.resume(dir);
+            }
+        } else
+            snake.keyPressed(e);
 
+        previousKeyCode = e.getKeyCode();
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        snake.keyReleased(e);
     }
 
     @Override
-    public void addUiComponents(JComponent target){
+    public void addUiComponents(JComponent target) {
 
     }
 
-    private void renderDrawables(Graphics g) {
-        for (Drawable obj : objects) {
-            drawObject(g, obj);
-        }
-    }
-
-    private void drawObject(Graphics g, Drawable dObject){
+    private void drawObject(Graphics g, Drawable dObject) {
         Graphics2D g2d = (Graphics2D) g;
         dObject.draw(g2d);
     }
 
-    private void drawPlayField(Graphics g){
+    private void drawPlayField(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
 
         g2d.setColor(Color.BLACK);
 
-        for(int i = 0; i <= PANEL_WIDTH; i+= 25)
+        for (int i = 0; i <= PANEL_WIDTH; i += snake.getWidth())
             g2d.drawLine(0, i, PANEL_WIDTH, i);
-        for(int i = 0; i <= PANEL_HEIGHT; i+= 25)
-            g2d.drawLine(i,0, i, PANEL_HEIGHT);
+        for (int i = 0; i <= PANEL_HEIGHT; i += snake.getHeight())
+            g2d.drawLine(i, 0, i, PANEL_HEIGHT);
 
-        g2d.drawString("Points: ", 0, 520);
+        g2d.drawString(pointStr + points, 0, 520);
+    }
+
+    private void generateFood() {
+        SnakeFood temp;
+        while (true) {
+            int foodX = ThreadLocalRandom.current().nextInt(0, 500);
+            int realX = foodX - foodX % squareSize + (squareSize - SnakeFood.FOOD_SIZE) / 2;
+            int foodY = ThreadLocalRandom.current().nextInt(0, 500);
+            int realY = foodY - foodY % squareSize + (squareSize - SnakeFood.FOOD_SIZE) / 2;
+            temp = new SnakeFood(realX, realY);
+            if (snake.intersects(temp)) continue;
+            else break;
+        }
+        foodItem = temp;
+        drawables.add(foodItem);
+    }
+
+    public int getPoints() {
+        return points;
     }
 }
