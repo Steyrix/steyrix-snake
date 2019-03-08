b39429e513e5decbbc71dc84ad731be99322b230
Georgy Pevnev <steyrixio@gmail.com>
2019-03-09 00:40:22 +0300

    created scene interface for managing different game states

---
+++ src/game/scenes/Scene.java
@@ -0,0 +1,20 @@
+package game.scenes;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public interface Scene {
+
+    void init();
+
+    void drawScene(Graphics g);
+
+    void keyPressed(KeyEvent e);
+
+    void keyReleased(KeyEvent e);
+
+    void actionPerformed();
+
+    void addUiComponents(JComponent target);
+}
